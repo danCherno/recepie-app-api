0f461b90b14fc25518136d17f3f0853654161fed
Cherno <daniel.cherno9@gmail.com>
2025-10-25 22:19:03 +0300

    setup django admin

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
